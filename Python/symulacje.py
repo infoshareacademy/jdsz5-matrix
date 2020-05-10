63c9d8042c7eba19c156a88878bc7c89738597a2
Maciej Gadaj <gadmacpg@gmail.com>
2020-05-10 17:11:59 +0200

    Dodano do pliku symulacje i app prawdopodobienstwo.

--- Python/symulacje.py
+++ Python/symulacje.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 import random
 from scipy.stats import ttest_ind
+from scipy.stats import poisson
 
 def srednia_miesiac(df_prognozy):
     monthly = df_prognozy.groupby(['data','MIESIAC']).count()['OBJECTID'].reset_index()
@@ -31,4 +32,16 @@ def testSrednich_bootstrap(df_prognozy, nr_miesiacaA, nr_miesiacaB, n=1000,alpha
         pvalues.append(p)
         
     print('Wynik testu istotny w {0} symulacji.'.format(  "{:.1%}".format(np.mean(pvalues) )))
-    
+
+def prawdopodobienstwo(k, mu):
+    prawdopA = poisson.pmf(k, mu)
+    print('Prawdopodpbieństwo, że w następnym tygodniu nie będzie kolizji w danym miejscu: {}'.format(prawdopA))
+
+    prawdopB = poisson.pmf(k+2, mu)
+    print('Prawdopodobieństwo, że będą dokładnie 2 kolizje w następnym tygodniu: {}'.format(prawdopB))
+
+    prawdopC = poisson.pmf(k, mu) + poisson.pmf(k+1, mu) + poisson.pmf(k+2, mu)
+    print('Prawdopodobieństwo, że będą maksymalnie 2 kolizje w następnym tygodniu: {}'.format(prawdopC))
+
+    prawdopD = 1 - (poisson.pmf(k, mu*2) + poisson.pmf(k+1, mu*2))
+    print('Prawdopodobieńśtwo, że w ciągu następnych 2 tygodni będą przynajmniej 2 kolizje: {}'.format(prawdopD))
