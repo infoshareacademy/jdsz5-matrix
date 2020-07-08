989f31e36f65be6419e82ac270fb79eee636dcbd
Joanna Kwiatkowska <joarie@wp.pl>
2020-07-09 01:42:49 +0200

    poprawki_do_funkcji_widgetu

--- ML/results/functions.py
+++ ML/results/functions.py
@@ -12,7 +12,7 @@ style = {'description_width': 'initial'}
 #słownik
 #miesiace = [(1, 'January'),(2,'February'),(3,'March'),(4,'April'),(5,'May'),(6,'June'),(7,'July'),(8,'August'),(9,'September'),(10,'October'),(11,'November'),(12,'December')]
 months = ['January','February','March','April','May','June','July','August','September','October','November','December']
-property = ['Apartment','Bed_Breakfast','Boat','Bungalow','Cabin','Camper_RV','Chalet','Condominium','Dorm','House','Loft','Other','Townhouse','Yurt']
+property = ['Apartment','Bed_Breakfast','Boat','Bungalow','Cabin','Camper_RV','Chalet','Condominium','House','Loft','Other','Townhouse','Yurt']
 room = ['Entire_Home_apt','Private_room','Shared_room']
 zip_code = ['98101','98102','98103','98104','98105','98106','98107','98108','98109','98112','98115','98116','98117','98118','98119','98121','98122','98125','98126','98133','98134','98136','98144','98146','98177','98178','98199']
 #clik_miesiac = widgets.IntSlider(description='Miesiąc:', value=1, min=1, max=12)
@@ -23,20 +23,20 @@ clik_month = widgets.SelectionSlider(description='Month:', options=months, value
 clik_property = widgets.Select(description='Property type:', options=property, style=style, rows=len(property))
 #clik_room = widgets.RadioButtons(description='Room type:', options=room)
 clik_room = widgets.ToggleButtons(description='Room type:', options=room)
-clik_guests = widgets.Checkbox(value=False, description='Guest included', disabled=False)
-clik_extra_people = widgets.Checkbox(value=False, description='Extra people', disabled=False)
-clik_cleaning = widgets.Checkbox(value=False, description='Cleanig fee', disabled=False)
-clik_superhost = widgets.Checkbox(value=False, description='Superhost', disabled=False)
-clik_zip_code = widgets.Select(description='Zip code:', options=zip_code, disabled=False, rows=14, layout=Layout(width='auto'))
-clik_bedrooms = widgets.IntSlider(description='Bedrooms:', value=1, min=1, max=5)
-clik_beds = widgets.IntSlider(description='Beds:', value=1, min=1, max=5)
-clik_bathrooms = widgets.IntSlider(description='Bathrooms:', value=1, min=1, max=5, style=style)
-clik_accommodates = widgets.IntSlider(description='Accommodates:', value=1, min=1, max=5, style=style)
+clik_guests = widgets.IntSlider(description='Guest included', value=0, min=0, max=10, style=style) # w danych jest max 7
+clik_extra_people = widgets.Checkbox(value=False, description='Extra people', disabled=False, style=style)
+clik_cleaning = widgets.IntSlider(description='Cleanig fee', value=50, min=1, max=200, style=style)
+clik_zip_code = widgets.Select(description='Zip code:', options=zip_code, disabled=False, rows=13, layout=Layout(width='auto'))
+clik_bedrooms = widgets.IntSlider(description='Bedrooms:', value=1, min=1, max=5, style=style) # w danych jest max 4
+clik_beds = widgets.IntSlider(description='Beds:', value=1, min=1, max=10, style=style) # w danych jest max 7 łóżek
+clik_bathrooms = widgets.IntSlider(description='Bathrooms:', value=1, min=0, max=5, style=style)  # w dnych jest max 3,5
+clik_accommodates = widgets.IntSlider(description='Accommodates:', value=1, min=1, max=10, style=style) # w danych jest max 7
+clik_nights = widgets.IntSlider(description='Minimum nights', value=1, min=1, max=31, style=style) 
 
 clik_model = widgets.ToggleButtons(description='ML MODEL:', options=['xgboost', 'random_forest'])
 
 def xgboost_model():
-    df = pd.read_csv('../data/data.csv')
+    df = pd.read_csv('../data/data_minus_outliers_20200708_min.csv')
     y = df.Price.values
     X = df.drop('Price', axis=1)
     X = pd.DataFrame(StandardScaler().fit_transform(X), columns = X.columns)
@@ -66,9 +66,9 @@ def xgboost_model():
 def formularz():
     # ostateczna ramka
     box_inside_left = widgets.HBox([clik_zip_code, clik_property])
-    left_box = widgets.VBox([clik_month, box_inside_left, clik_room])
+    left_box = widgets.VBox([clik_model, box_inside_left, clik_room])
     #left_box = widgets.VBox([clik_zip_code, clik_month, clik_room, clik_property])
-    right_box = widgets.VBox([clik_guests, clik_extra_people, clik_cleaning, clik_superhost, clik_accommodates, clik_beds, clik_bedrooms, clik_bathrooms])
+    right_box = widgets.VBox([clik_month, clik_nights, clik_cleaning, clik_accommodates, clik_beds, clik_bedrooms, clik_bathrooms, clik_guests, clik_extra_people])
     box = widgets.HBox([left_box, right_box])
     #pokaż
     display(box)
@@ -86,9 +86,10 @@ def click_button_moj(button_moj):
     df_preds['cleaning_fee'] = clik_cleaning.value
     df_preds['guests_included'] = clik_guests.value
     df_preds['extra_people'] = clik_extra_people.value
+    df_preds['minimum_nights'] = clik_nights.value
     print(f'\nZip code: {clik_zip_code.value}, Month: {clik_month.value}, Property type: {clik_property.value}, Room type: {clik_room.value}')
-    print(f'accommodates: {clik_accommodates.value}, bathrooms: {clik_bathrooms.value}, bedrooms: {clik_bedrooms.value}, beds: {clik_beds.value}')
-    print(f'cleaning_fees: {clik_cleaning.value}, guests_included: {clik_guests.value}, extra_people: {clik_extra_people.value}')
+    print(f'Minimum nights: {clik_nights.value}, Accommodates: {clik_accommodates.value}, Bathrooms: {clik_bathrooms.value}, Bedrooms: {clik_bedrooms.value}, Beds: {clik_beds.value}')
+    print(f'Cleaning_fees: {clik_cleaning.value}, Guests_included: {clik_guests.value}, Extra_people: {clik_extra_people.value}')
     xgboost_calosc(df_preds)
 
 def oblicz_cene(model):
@@ -104,9 +105,10 @@ def oblicz_cene(model):
     df_preds['cleaning_fee'] = clik_cleaning.value
     df_preds['guests_included'] = clik_guests.value
     df_preds['extra_people'] = clik_extra_people.value
+    df_preds['minimum_nights'] = clik_nights.value
     print(f'\nZip code: {clik_zip_code.value}, Month: {clik_month.value}, Property type: {clik_property.value}, Room type: {clik_room.value}')
-    print(f'accommodates: {clik_accommodates.value}, bathrooms: {clik_bathrooms.value}, bedrooms: {clik_bedrooms.value}, beds: {clik_beds.value}')
-    print(f'cleaning_fees: {clik_cleaning.value}, guests_included: {clik_guests.value}, extra_people: {clik_extra_people.value}')
+    print(f'Minimum nights: {clik_nights.value}, Accommodates: {clik_accommodates.value}, Bathrooms: {clik_bathrooms.value}, Bedrooms: {clik_bedrooms.value}, Beds: {clik_beds.value}')
+    print(f'Cleaning_fees: {clik_cleaning.value}, Guests_included: {clik_guests.value}, Extra_people: {clik_extra_people.value}')
     xgboost_check(df_preds, model)
 
 def xgboost_check(df_preds, model):
@@ -115,7 +117,7 @@ def xgboost_check(df_preds, model):
 
 
 def xgboost_calosc(df_preds):
-    df = pd.read_csv('../data/data.csv')
+    df = pd.read_csv('../data/data_minus_outliers_20200708_min.csv')
     y = df.Price.values
     X = df.drop('Price', axis=1)
     X = pd.DataFrame(StandardScaler().fit_transform(X), columns = X.columns)
